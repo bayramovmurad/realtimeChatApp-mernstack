f6635c47e4e02a41f7b4514152a31679c70a5d4a
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-02-13 14:49:47 +0400

    complete message and convercation schema and function token cookie

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,11 +1,39 @@
-export const messages = (req, res) => {
+import Conversation from '../models/conversationModel.js';
+import Message from '../models/messageModel.js';
+
+export const messages = async (req, res) => {
     try {
         const { message } = req.body;
-        const { id } = req.params.id;
-        const senderId = req.userId;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] }
+        });
+
+        if (!conversation) {
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId],
+            })
+        };
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message
+        });
+
+        if (newMessage) {
+            conversation.messages.push(newMessage._id);
+        };
+
+        res.status(201).json(newMessage);
+
+        await conversation.save();
+        await newMessage.save();
 
     } catch (error) {
         console.log("Error in send message controller", error.message);
-        res.status(500).json({ error: "Internal server error" });
+        res.status(500).json({ error: "Internal server error" })
     }
 };
