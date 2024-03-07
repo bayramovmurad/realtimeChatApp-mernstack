9ddce22cf8e46134f23c1434d594fec24fc8c127
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2024-03-07 21:05:48 +0400

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
-import databaseMongoDb from './db/database.js';
-import messageRoutes from './routes/messageRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import cookieParser from 'cookie-parser';
-import {app,server} from './socket/socket.js'
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-const PORT = process.env.PORT || 8000
-dotenv.config();
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/users', userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
-    databaseMongoDb();
-    console.log(`server running on port ${PORT}`);
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`);
 });
